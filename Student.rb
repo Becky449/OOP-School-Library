0bff45568b32e8d83f94d065e36a849d72632b71
Becky449 <rabnab12@gmail.com>
2023-04-10 16:34:34 +0300

    fix errors

--- Student.rb
+++ Student.rb
@@ -1,13 +1,13 @@
-Require "./Person.rb"
+Require './person.rb'
 
 class Student < Person
-    attr_accessor :classroom
-
-    def initialize(age, classroom, name = "Unknown", parent_permission = true)
-        super(age, name, parent_permission)
-        @classroom = classroom
-    end
-    def play_hooky
-        "¯\(ツ)/¯"
-    end
+  attr_accessor :classroom
+  
+  def initialize(age, classroom, name = "Unknown", parent_permission: true)
+    super(age, name, parent_permission)
+    @classroom = classroom
+  end
+  def play_hooky
+    "¯\(ツ)/¯"
+  end
 end
--- Teacher.rb
+++ Teacher.rb
@@ -1,14 +1,13 @@
-Require "./Person.rb"
+Require './person.rb'
 
 class Teacher < Person
-    attr_accessor :specialization
+  attr_accessor :specialization
 
-    def initialize(age, specialization, name = "Unknown", parent_permission = true)
-        super(age, name, parent_permission)
-        @specialization = specialization
-    end
-
-    def can_use_services?
-        true
-    end
+  def initialize(age, specialization, name = "Unknown", parent_permission: true)
+    super(age, name, parent_permission)
+    @specialization = specialization
+  end
+  def can_use_services?
+    true
+  end
 end
